1c73ecf99346652249b9efc73fd9d5405017cfa9
fernandacornejom <fernandacornejom@gmail.com>
2022-05-10 21:48:56 -0400

    todo ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   def index
   end
 
+  def dashboard
+    @users = User.find_valid 
+  end
+
   def create
     @user = User.create(email: params[:email], age: params[:age], comment: params[:comment])
   end
